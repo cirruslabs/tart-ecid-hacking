7a74e7690724126b40b13f0999cd5785c0db851e
fedor <fedor.korotkov@gmail.com>
2023-08-29 07:27:05 -0400

    parametrize things

--- main.go
+++ main.go
@@ -5,19 +5,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 )
 
 func main() {
-	tartBinaryPath := "/Users/fedor/workspace/tart/.build/debug/tart"
+	vmName := "ventura-base"
+	outputFile := "data.cvs"
 
-	f, err := os.Create("data.cvs")
+	args := os.Args[1:]
+	if len(args) > 1 {
+		outputFile = args[1]
+	}
+	if len(args) > 0 {
+		vmName = args[0]
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tartBinaryPath := path.Join(homeDir, "workspace", "tart", ".build", "debug", "tart")
+
+	f, err := os.Create(outputFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	for i := 0; i < 2_000; i++ {
-		info, err := collectForVM(context.Background(), tartBinaryPath, "ventura-xcode")
+	for i := 0; i < 50_000; i++ {
+		info, err := collectForVM(context.Background(), tartBinaryPath, vmName)
 		if err != nil {
 			panic(err)
 		}
