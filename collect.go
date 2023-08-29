f490792f05461a1f632c191483f7ad6b25fab119
fedor <fedor.korotkov@gmail.com>
2023-08-29 07:30:50 -0400

    One script file

--- collect.go
+++ collect.go
@@ -7,8 +7,11 @@ import (
 	"fmt"
 	"github.com/avast/retry-go"
 	"golang.org/x/crypto/ssh"
+	"log"
 	"net"
+	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"time"
 )
@@ -16,6 +19,42 @@ import (
 type TartInfo struct {
 	ECID                string
 	HardwareModelBase64 string
+}
+
+func main() {
+	vmName := "ventura-base"
+	outputFile := "data.cvs"
+
+	args := os.Args[1:]
+	if len(args) > 1 {
+		outputFile = args[1]
+	}
+	if len(args) > 0 {
+		vmName = args[0]
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tartBinaryPath := path.Join(homeDir, "workspace", "tart", ".build", "debug", "tart")
+
+	f, err := os.Create(outputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	for i := 0; i < 50_000; i++ {
+		info, err := collectForVM(context.Background(), tartBinaryPath, vmName)
+		if err != nil {
+			panic(err)
+		}
+		line := fmt.Sprintf("%s\t%s", info.ECID, info.HardwareModelBase64)
+		println(line)
+		_, _ = f.WriteString(line + "\n")
+	}
 }
 
 func collectForVM(ctx context.Context, tartBinaryPath, vmName string) (*TartInfo, error) {
